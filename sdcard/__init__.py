62461a500e504f25d0b3af5fb639eec50d589d17
cristi2005 <pc_cristi@yahoo.com>
2026-09-01 14:44:03 +0300

    Update __init__.py

--- sdcard/__init__.py
+++ sdcard/__init__.py
@@ -1,12 +1,19 @@
-# Componenta de card SD pentru ESP32-A1S.
+# Componenta de card SD pentru ESP32-A1S: monteaza cardul si serveste fisierele de pe el
+# printr-un mic server web pornit chiar pe placa.
 #
-# ESPHome NU are asa ceva in mod normal: driverul de card si sistemul de fisiere FAT sunt
-# scoase dinadins din compilare, ca sa nu ocupe loc degeaba. Aici le cerem inapoi, prin cele
-# patru "include_builtin_idf_component" de mai jos - fara ele, codul nici nu s-ar lega.
+# DE CE UN SERVER WEB, si nu citire directa din fisier: biblioteca de decodare folosita de
+# ESPHome (micro-decoder) stie sa porneasca doar din doua locuri - o adresa http:// sau un
+# fisier incarcat INTREG in memorie. Nu are varianta cu fisier de pe disc. Verificat in lista
+# ei de metode publice, nu presupus. Servindu-le prin http://127.0.0.1, fisierele de pe card
+# trec prin exact acelasi drum ca un post de radio, deci pastram egalizatorul, vu-metrul,
+# volumul si toate butoanele care merg deja.
 #
-# Pinii NU se pot alege: pe ESP32 cititorul de card e legat direct in siliciu la GPIO 14 (ceas),
-# 15 (comenzi) si 2 (date). Restul (4, 12, 13) sunt folositi doar in modul pe 4 fire, pe care
-# nu-l folosim tocmai ca sa nu ne certam cu butonul KEY2 de pe GPIO13.
+# ESPHome scoate dinadins din compilare driverul de card, protocolul SD si sistemul de fisiere
+# FAT. Aici le cerem inapoi - fara asta codul nici nu s-ar lega.
+#
+# Pinii NU se pot alege: pe ESP32 cititorul de card e legat in siliciu la GPIO 14 (ceas),
+# 15 (comenzi) si 2 (date). Mergem pe 1 fir ca sa nu avem nevoie de GPIO 4, 12 si 13 - dintre
+# care 13 e butonul KEY2.
 
 import esphome.codegen as cg
 from esphome.components.esp32 import (
@@ -22,6 +29,8 @@ CODEOWNERS = ["@cristi2005"]
 DEPENDENCIES = ["esp32"]
 
 CONF_MOUNT_POINT = "mount_point"
+CONF_MUSIC_FOLDER = "music_folder"
+CONF_HTTP_PORT = "http_port"
 
 sdcard_ns = cg.esphome_ns.namespace("sdcard")
 SDCard = sdcard_ns.class_("SDCard", cg.Component)
@@ -30,6 +39,9 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(SDCard),
         cv.Optional(CONF_MOUNT_POINT, default="/sd"): cv.string,
+        cv.Optional(CONF_MUSIC_FOLDER, default="/sd"): cv.string,
+        # 0 opreste serverul. Nu pune 80 - acolo sta deja pagina web a placii.
+        cv.Optional(CONF_HTTP_PORT, default=81): cv.port,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -37,9 +49,17 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config: ConfigType) -> None:
     require_fatfs()
     require_vfs_dir()
-    for idf_component in ("fatfs", "sdmmc", "esp_driver_sdmmc", "wear_levelling"):
+    for idf_component in (
+        "fatfs",
+        "sdmmc",
+        "esp_driver_sdmmc",
+        "wear_levelling",
+        "esp_http_server",
+    ):
         include_builtin_idf_component(idf_component)
 
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     cg.add(var.set_mount_point(config[CONF_MOUNT_POINT]))
+    cg.add(var.set_music_folder(config[CONF_MUSIC_FOLDER]))
+    cg.add(var.set_http_port(config[CONF_HTTP_PORT]))
