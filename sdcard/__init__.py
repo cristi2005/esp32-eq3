923aec54d533e41f07362f526c2548e48fac1a66
cristi2005 <pc_cristi@yahoo.com>
2026-09-04 14:20:44 +0300

    Update __init__.py

--- sdcard/__init__.py
+++ sdcard/__init__.py
@@ -1,15 +1,16 @@
 import esphome.codegen as cg
+from esphome.components import speaker
 from esphome.components.esp32 import (
     include_builtin_idf_component,
     require_fatfs,
     require_vfs_dir,
 )
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_SPEAKER
 from esphome.types import ConfigType
 
 CODEOWNERS = ["@cristi2005"]
-DEPENDENCIES = ["esp32"]
+DEPENDENCIES = ["esp32", "speaker"]
 AUTO_LOAD = ["audio"]
 
 CONF_MOUNT_POINT = "mount_point"
@@ -23,6 +24,9 @@ CONFIG_SCHEMA = cv.Schema(
         cv.GenerateID(): cv.declare_id(SDCard),
         cv.Optional(CONF_MOUNT_POINT, default="/sd"): cv.string,
         cv.Optional(CONF_MUSIC_FOLDER, default="/sd"): cv.string,
+        # NOU: speakerul-sursa (ex: "sursa_muzica") catre care play_track_streaming() trimite
+        # DIRECT sunetul decodat, bucata cu bucata - vezi comentariul mare din sdcard.h.
+        cv.Required(CONF_SPEAKER): cv.use_id(speaker.Speaker),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -43,3 +47,5 @@ async def to_code(config: ConfigType) -> None:
     await cg.register_component(var, config)
     cg.add(var.set_mount_point(config[CONF_MOUNT_POINT]))
     cg.add(var.set_music_folder(config[CONF_MUSIC_FOLDER]))
+    spk = await cg.get_variable(config[CONF_SPEAKER])
+    cg.add(var.set_speaker(spk))
